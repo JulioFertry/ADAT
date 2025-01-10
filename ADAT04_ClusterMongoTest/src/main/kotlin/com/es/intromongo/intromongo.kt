7338f96edc1360cbeaf0ba46d221b577f75d5f4f
JulioFertry <juliofertry@gmail.com>
2025-01-10 21:35:23 +0100

    clusterTest

---
+++ ADAT04_ClusterMongoTest/src/main/kotlin/com/es/intromongo/intromongo.kt
@@ -0,0 +1,33 @@
+package com.es.intromongo
+
+import com.mongodb.client.MongoClient
+import com.mongodb.client.MongoClients
+import io.github.cdimascio.dotenv.dotenv
+
+
+fun main() {
+
+    // 1º Realizar la conexión con MongoDB (Cluster)
+
+    // declaro un objeto para usar las clases de dotenv
+    val dotenv = dotenv()
+
+    // guardo en una variable la url de conexión
+    val urlConnectionMongo = dotenv["URL_MONGODB"]
+
+    // podemos realizar la conexión con el cluster
+    val cluster: MongoClient = MongoClients.create(urlConnectionMongo)
+
+    // 2º Nos conectamos a la base de datos
+    val bd = cluster.getDatabase("adaprueba")
+
+    // 3º Con la BD, podemos realizar las consultas a la coleccion que queramos
+    val collection = bd.getCollection("documentoada")
+
+    val docConsulta = collection.find()
+
+    docConsulta.forEach {doc ->
+        println(doc.toString())
+    }
+
+}
