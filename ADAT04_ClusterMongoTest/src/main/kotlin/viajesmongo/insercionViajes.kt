76fad151bc463edfa36581af2685a32d1bd28eed
JulioFertry <juliofertry@gmail.com>
2025-01-15 17:15:37 +0100

    insertar viaje

--- ADAT04_ClusterMongoTest/src/main/kotlin/viajesmongo/insercionViajes.kt
+++ ADAT04_ClusterMongoTest/src/main/kotlin/viajesmongo/insercionViajes.kt
@@ -3,6 +3,34 @@ package viajesmongo
 import com.mongodb.client.MongoClient
 import com.mongodb.client.MongoClients
 import io.github.cdimascio.dotenv.dotenv
+import org.bson.Document
+import java.time.Instant
+
+
+fun createNewDocument(): Document {
+    try {
+        print("Introduce la ciudad de origen: ")
+        val origen = readln()
+        print("Introduce la ciudad de destino: ")
+        val destino = readln()
+        print("Introduce el precio: ")
+        val precio = readln().toDouble()
+        print("Introduce el numero del mes: ")
+        val mes = readln().toInt()
+        print("Introduce el numero del dia: ")
+        val dia = readln()
+        val fecha = "2025-${mes}-${dia}T10:30:00.000Z"
+        val newDoc = Document()
+            .append("origen", origen)
+            .append("destino", destino)
+            .append("precio", precio)
+            .append("fecha_salida", Instant.parse(fecha))
+        return newDoc
+    } catch (e: Exception){
+        throw IllegalArgumentException("Error al introducir los datos")
+    }
+
+}
 
 
 fun insert() {
@@ -13,5 +41,19 @@ fun insert() {
     val mongoClient: MongoClient = MongoClients.create(connectString)
     val databaseName = "viajesbd"
 
-    
+    try {
+        val database = mongoClient.getDatabase(databaseName)
+        val collection = database.getCollection("reservas_viaje")
+        val nuevoDocumento = createNewDocument()
+        collection.insertOne(nuevoDocumento)
+
+    } catch (e: IllegalArgumentException) {
+        println(e)
+    } catch (e: Exception) {
+        println("Error al conectar a MongoDB: ${e.message}")
+    } finally {
+        // Cerrar la conexión
+        mongoClient.close()
+    }
+
 }
---
+++ ADAT04_ClusterMongoTest/src/main/kotlin/viajesmongo/mainViajes.kt
@@ -0,0 +1,5 @@
+package viajesmongo
+
+fun main() {
+    insert()
+}
