7d7aff45e276da7c74ea3bf963c47f039c747072
JulioFertry <juliofertry@gmail.com>
2025-02-03 18:03:19 +0100

    model

--- ADAT04_Noticias/src/main/kotlin/model/Address.kt
+++ ADAT04_Noticias/src/main/kotlin/model/Address.kt
@@ -2,8 +2,8 @@ package org.example.model
 
 import java.util.Date
 
-data class Commentary(
-    val userName: String,
+data class Address(
+    val user: String,
     val report: String,
     val commentary: String,
     val date: Date
--- ADAT04_Noticias/src/main/kotlin/model/User.kt
+++ ADAT04_Noticias/src/main/kotlin/model/User.kt
@@ -1,3 +1,13 @@
 package org.example.model
 
-data class User()
+import org.bson.codecs.pojo.annotations.BsonProperty
+
+data class User(
+    val email: String,
+    val name: String,
+    val nickname: String,
+    val state: Boolean,
+    @BsonProperty("telefonos")
+    val phoneNumbers: List<String>,
+    val address: Address
+)
